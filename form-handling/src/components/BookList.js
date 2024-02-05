ba8d67eae7295c8af46215012142afb464fba243
Kris French <frenchie43972@gmail.com>
2024-02-06 06:08:28 +0900

    cool

--- form-handling/src/components/BookList.js
+++ form-handling/src/components/BookList.js
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import BooksContext from '../context/books';
 import BookShow from './BookShow';
 
 function BookList({books, onDelete, onEdit}) {
+  const value = useContext(BooksContext);
+
   const renderedBooks = books.map((book) =>{
     return (
       <BookShow onEdit={onEdit} key={book.id} book={book} onDelete={onDelete} />
@@ -8,7 +12,10 @@ function BookList({books, onDelete, onEdit}) {
   });
 
   return (
-    <div className='book-list'>{renderedBooks}</div>
+    <div className='book-list'>
+      {value}
+      {renderedBooks}
+    </div>
   );
 }
 
